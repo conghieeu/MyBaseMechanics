123088c67946ea2e178c23c440245f08b3f33ca9
conghieeu <63394751+conghieeu@users.noreply.github.com>
2024-11-07 01:49:43 +0700

    add gitignore

---
+++ Assets/_Runtime/SimpleProceduralMesh/Scripts/SimpleProceduralMesh.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace HieuDev
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class SimpleProceduralMesh : MonoBehaviour
+    {
+        
+    }
+}
+
